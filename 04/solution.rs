db1ec55bb8594fe408c3ce8f7d7c41c414c092ff
Mats01 <matej.butkovic@gmail.com>
2024-12-04 11:57:35 +0100

    day 4 part 2

--- 04/solution.rs
+++ 04/solution.rs
@@ -79,6 +79,36 @@ fn check_down_left(grid: &Vec<Vec<String>>, start: [i32; 2]) -> bool {
     check_direction(grid, start, [1, -1])
 }
 
+
+fn check_x(grid: &Vec<Vec<String>>, start: [usize; 2]) -> bool {
+
+    if start[0] < 1 || start[1] < 1 || start[0] > (grid.len() - 2) || start[1] > (grid[0].len() - 2) {
+        return false;
+    }
+
+
+    // check if above left and above right is M
+    let above_left = &grid[start[0] - 1][start[1] - 1];
+    let above_right = &grid[start[0] - 1][start[1] + 1];
+    let below_left = &grid[start[0] + 1][start[1] - 1];
+    let below_right = &grid[start[0] + 1][start[1] + 1];
+    
+    let positions = vec![above_left, above_right, below_left, below_right];
+
+    let allowed_combinations = vec![["M", "S", "M", "S"], ["M", "M", "S", "S"], ["S", "S", "M", "M"], ["S", "M", "S", "M"]];
+
+
+    for combination in allowed_combinations {
+        if positions == combination {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+
 fn main() {
     let lines = read_lines();
 
@@ -90,50 +120,52 @@ fn main() {
 
 
 
-    println!("grid: {:?}", grid);
+
     let mut total_finds = 0;
+
+    let mut total_xs = 0;
 
     for i in 0..grid.len() as i32{
         for j in 0..grid[0].len() as i32{
             if grid[i as usize][j as usize] == "X" {
                 if check_right(&grid, [i, j]) {
-                    println!("Found at {:?}", [i, j]);
                     total_finds += 1;
                 }
                 if check_left(&grid, [i, j]) {
-                    println!("Found at {:?}", [i, j]);
                     total_finds += 1;
                 }
                 if check_up(&grid, [i, j]) {
-                    println!("Found at {:?}", [i, j]);
                     total_finds += 1;
                 }
                 if check_down(&grid, [i, j]) {
-                    println!("Found at {:?}", [i, j]);
                     total_finds += 1;
                 }
                 if check_down_right(&grid, [i, j]) {
-                    println!("Found at {:?}", [i, j]);
                     total_finds += 1;
                 }
                 if check_up_right(&grid, [i, j]) {
-                    println!("Found at {:?}", [i, j]);
                     total_finds += 1;
                 }
                 if check_up_left(&grid, [i, j]) {
-                    println!("Found at {:?}", [i, j]);
                     total_finds += 1;
                 }
                 if check_down_left(&grid, [i, j]) {
-                    println!("Found at {:?}", [i, j]);
                     total_finds += 1;
                 }
             }
+            if grid[i as usize][j as usize] == "A" {
+                if check_x(&grid, [i as usize, j as usize]) {
+                    total_xs += 1;
+                }
+               
+            }
+
         }
     }
 
     println!("Total finds: {}", total_finds);
 
+    println!("Total xs: {}", total_xs);
 
 
 }
