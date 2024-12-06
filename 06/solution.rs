c4701d3a7b17e028cd0e209624a51edff0c72c1c
Mats01 <matej.butkovic@gmail.com>
2024-12-06 18:18:24 +0100

    day 6 part 2

--- 06/solution.rs
+++ 06/solution.rs
@@ -2,7 +2,6 @@ use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-
 fn read_lines() -> Vec<String> {
     let args: Vec<String> = env::args().collect();
 
@@ -24,19 +23,17 @@ fn read_lines() -> Vec<String> {
     lines
 }
 
-
-
-
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum TileType {
     Obstacle,
     Empty,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Tile {
     tile_type: TileType,
     visits: i32,
+    last_visit_direction: Option<(i32, i32)>,
 }
 
 #[derive(Debug)]
@@ -45,20 +42,27 @@ struct Guard {
     direction: (i32, i32),
 }
 
+#[derive(Debug, PartialEq)]
+enum MoveResult {
+    Moved,
+    Turned,
+    Exited,
+    Stuck,
+}
 
-
-
-fn move_guard(guard: &mut Guard, tiles: &mut Vec<Vec<Tile>>) -> bool {
-    let new_position = (guard.position.0 + guard.direction.0, guard.position.1 + guard.direction.1);
-
-
+fn move_guard(guard: &mut Guard, tiles: &mut Vec<Vec<Tile>>) -> MoveResult {
+    let new_position = (
+        guard.position.0 + guard.direction.0,
+        guard.position.1 + guard.direction.1,
+    );
 
     // check if the new position would go out of bounds
     let rows = tiles.len() as i32;
     let cols = tiles[0].len() as i32;
 
-    if new_position.0 >= cols || new_position.1 >= rows || new_position.0 < 0 || new_position.1 < 0 {
-        return false;
+    if new_position.0 >= cols || new_position.1 >= rows || new_position.0 < 0 || new_position.1 < 0
+    {
+        return MoveResult::Exited;
     }
 
     if tiles[new_position.1 as usize][new_position.0 as usize].tile_type == TileType::Obstacle {
@@ -70,43 +74,74 @@ fn move_guard(guard: &mut Guard, tiles: &mut Vec<Vec<Tile>>) -> bool {
 
         let directions = [(0, -1), (1, 0), (0, 1), (-1, 0)];
 
-        guard.direction = directions[(directions.iter().position(|&d| d == guard.direction).unwrap() + 1) % 4];
-        return true;
+        guard.direction = directions[(directions
+            .iter()
+            .position(|&d| d == guard.direction)
+            .unwrap()
+            + 1)
+            % 4];
+        return MoveResult::Turned;
     }
 
     guard.position = new_position;
-    tiles[new_position.1 as usize][new_position.0 as usize].visits += 1;
-    return true;
+
+    let new_tile = &mut tiles[new_position.1 as usize][new_position.0 as usize];
+
+    if new_tile.visits > 0 && new_tile.last_visit_direction == Some(guard.direction) {
+        return MoveResult::Stuck;
+    }
+
+    new_tile.visits += 1;
+    new_tile.last_visit_direction = Some(guard.direction);
+    return MoveResult::Moved;
 }
-
-
 
 fn main() {
     let lines = read_lines();
 
+    let mut initial_position = (0, 0);
+    let initial_direction = (0, -1);
 
-   
+    let mut guard = Guard {
+        position: (0, 0),
+        direction: (0, -1),
+    };
 
-    let mut guard = Guard { position: (0, 0), direction: (0, -1) };
- 
     let mut tiles: Vec<Vec<Tile>> = Vec::new();
     for (y, line) in lines.iter().enumerate() {
         let mut row: Vec<Tile> = Vec::new();
         for (x, c) in line.chars().enumerate() {
-            let mut new_tile = Tile { tile_type: if c == '#' { TileType::Obstacle } else { TileType::Empty }, visits: 0 };
+            let mut new_tile = Tile {
+                tile_type: if c == '#' {
+                    TileType::Obstacle
+                } else {
+                    TileType::Empty
+                },
+                visits: 0,
+                last_visit_direction: None,
+            };
             if c == '^' {
                 guard.position = (x as i32, y as i32);
+                initial_position = (x as i32, y as i32);
                 new_tile.visits = 1;
+                new_tile.last_visit_direction = Some(guard.direction);
             }
             row.push(new_tile);
         }
         tiles.push(row);
     }
 
-    while move_guard(&mut guard, &mut tiles) {}
+    let mut copied_tiles = tiles.clone();
+
+    loop {
+        let result = move_guard(&mut guard, &mut copied_tiles);
+        if result == MoveResult::Exited {
+            break;
+        }
+    }
 
     let mut visited_tiles = 0;
-    for row in &tiles {
+    for row in &copied_tiles {
         for tile in row {
             if tile.visits > 0 {
                 visited_tiles += 1;
@@ -114,6 +149,31 @@ fn main() {
         }
     }
 
-    println!("Visited tiles: {}", visited_tiles);
+    println!("part 1: {}", visited_tiles);
 
+    let mut total_loops = 0;
+    for (y, row) in tiles.iter().enumerate() {
+        for (x, tile) in row.iter().enumerate() {
+            if tile.tile_type != TileType::Obstacle {
+                copied_tiles = tiles.clone();
+                copied_tiles[y][x].tile_type = TileType::Obstacle;
+
+                // reset guard position
+                guard.position = initial_position;
+                guard.direction = initial_direction;
+                loop {
+                    let result = move_guard(&mut guard, &mut copied_tiles);
+                    if result == MoveResult::Exited {
+                        break;
+                    }
+                    if result == MoveResult::Stuck {
+                        total_loops += 1;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    println!("part 2: {}", total_loops);
 }
