814564570117eb4a70e9e989147bc96471ab07cd
Mats01 <matej.butkovic@gmail.com>
2024-12-07 15:25:56 +0100

    day 7 part 2

--- 07/solution.rs
+++ 07/solution.rs
@@ -23,12 +23,29 @@ fn read_lines() -> Vec<String> {
     lines
 }
 
+fn permutation_generator(length: usize, digits: Vec<char>) -> Vec<Vec<char>> {
+    if length == 0 {
+        return vec![vec![]];
+    }
+    
+    let prev_perms = permutation_generator(length - 1, digits.clone());
+    prev_perms.iter()
+        .flat_map(|perm| {
+            digits.iter().map(move |&d| {
+                let mut new_perm = perm.clone();
+                new_perm.push(d);
+                new_perm
+            })
+        })
+        .collect()
+}
+
 
 fn main() {
     let lines = read_lines();
 
     let mut sum = 0;
-    for line in lines {
+    for line in &lines {
         let (target, numbers) = line.split_once(": ").unwrap();
         let target: i64 = target.parse().unwrap();
         let numbers: Vec<i64> = numbers.split(" ").map(|n| n.parse().unwrap()).collect();
@@ -66,6 +83,52 @@ fn main() {
 
         
     }
-    println!("{}", sum);
+    println!("part 1: {}", sum);
+
+
+    let mut sum2 = 0;
+
+    for line in &lines {
+        let (target, numbers) = line.split_once(": ").unwrap();
+        let target: i64 = target.parse().unwrap();
+        let numbers: Vec<i64> = numbers.split(" ").map(|n| n.parse().unwrap()).collect();
+         // make an array of all comibantions of 0, 1, 2 of length numbers.len() - 1
+         let combinations = permutation_generator(numbers.len() - 1, vec!['0', '1', '2']);
+         
+
+         for ternary_digits in combinations {
+            
+
+            let mut curr_value = numbers[0];
+            for j in 1..numbers.len() {
+                let digit = ternary_digits[j-1];
+                if digit == '1' {
+                    curr_value *= numbers[j];
+                } else if digit == '0' {
+                    curr_value += numbers[j];
+                } else if digit == '2' {
+                    // turn cur_value to string
+                    let s = curr_value.to_string();
+                    // turn number to string
+                    let n = numbers[j].to_string();
+                    // put the strings together
+                    let res = s + &n;
+                    curr_value = res.parse::<i64>().unwrap();
+                }
+
+                if curr_value > target {
+                    break;
+                }
+            }
+
+
+            if curr_value == target {
+                // sum all the numbers
+                sum2 += target;
+                break;
+            }
+        }
+    }
+    println!("part 2: {}", sum2);
 
 }
